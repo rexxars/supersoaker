bc11105408780010d8e3dd8a082066678c3914e3
Espen Hovlandsdal <rexxars@gmail.com>
2015-05-26 15:27:48 +0200

    Super soak!

--- README.md
+++ README.md
@@ -5,21 +5,15 @@ Add custom npm registry + user credentials to npmrc based on environment variabl
 ## Installing
 
 ```
-npm install --save supersoaker
+npm install -g supersoaker
 ```
 
 ## Basic usage
 
-```js
-var supersoaker = require('supersoaker');
-
-// To be documented...
-```
-
-## Testing
-
 ```bash
-git clone git@github.schibsted.io:vg/supersoaker.git && cd supersoaker
-npm install
-npm test
+$ supersoaker
 ```
+
+## License
+
+MIT-licensed. See LICENSE.
--- src/supersoaker.js
+++ src/supersoaker.js
@@ -1,7 +1,98 @@
+#!/usr/bin/env node
+
+/* eslint no-console: 0, no-process-exit: 0 */
 'use strict';
 
-function Supersoaker() {
-    return 'something';
+var npm = require('npm');
+var yargs = require('yargs');
+
+var defaultRegistry = 'https://registry.npmjs.org';
+var args = yargs
+    .usage('Usage: $0 [options]')
+    .command('squirt', 'Squirt the passed details into your npmrc file')
+    .alias('r', 'registry')
+    .nargs('r', 1)
+    .describe('r', 'Registry URL')
+    .alias('u', 'user')
+    .nargs('u', 1)
+    .describe('u', 'Username')
+    .alias('p', 'password')
+    .nargs('p', 1)
+    .describe('p', 'Password')
+    .alias('e', 'email')
+    .nargs('e', 1)
+    .describe('e', 'User email')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+var registry = args.registry || process.env.NPM_REGISTRY,
+    user = args.user || process.env.NPM_USER,
+    pass = args.password || process.env.NPM_PASSWORD,
+    email = args.email || process.env.NPM_EMAIL;
+
+if (!user) {
+    console.error('User not specified through CLI/environment vars');
+    process.exit(1);
 }
 
-module.exports = Supersoaker;
+if (!pass) {
+    console.error('Passworrd not specified through CLI/environment vars');
+    process.exit(1);
+}
+
+if (!email) {
+    console.error('Email not specified through CLI/environment vars');
+    process.exit(1);
+}
+
+npm.load({}, function(loadErr) {
+    if (loadErr) {
+        throw loadErr;
+    }
+
+    if (registry) {
+        npm.config.set('registry', registry, 'user');
+    }
+
+    npm.config.set('email', email, 'user');
+
+    var reg = registry || defaultRegistry;
+    var auth = {
+        username: user,
+        password: pass,
+        email: email
+    };
+
+    npm.registry.adduser(reg, { auth: auth }, function(err, doc) {
+        if (err && err.statusCode === 409) {
+            console.error('User already exists - incorrect password?');
+            process.exit(1);
+        }
+
+        if (err && [401, 403].indexOf(err.statusCode) > -1) {
+            console.error('Incorrect username/password');
+            process.exit(1);
+        }
+
+        if (err) {
+            throw err;
+        }
+
+        if (doc && doc.token) {
+            npm.config.setCredentialsByURI(reg, {
+                token: doc.token
+            });
+        } else {
+            console.error('No auth token received from npm.adduser');
+            process.exit(1);
+        }
+
+        npm.config.save('user', function(saveErr) {
+            if (saveErr) {
+                throw saveErr;
+            }
+        });
+    });
+});
+
